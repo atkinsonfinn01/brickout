4b0486c0e14f5b409b44da357110ede724aadcdf
atkinsonfinn01 <atkinsonfinn01@gmail.com>
2021-01-12 23:04:25 +0000

    Upload from computer

---
+++ Breakout/Assets/Scripts/Ball.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        GoBall();
+    }
+
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.collider.CompareTag("Bat"))
+        {
+            Vector2 vel;
+            vel.y = rb2d.velocity.y;
+            vel.x = (rb2d.velocity.x) + (coll.collider.attachedRigidbody.velocity.x);
+            print(vel.x);
+            print(vel.y);
+            rb2d.velocity = vel;
+        }
+        else if (coll.collider.CompareTag("Block"))
+        {
+            Destroy(coll.collider.gameObject);
+            //Add Score
+        }
+        //Else If Ground
+        //ResetGameFunction
+    }
+
+    void GoBall()
+    {
+        float rand = Random.Range(0, 2);
+        if (rand < 1)
+        {
+            rb2d.AddForce(new Vector2(30, -15));
+        }
+        else
+        {
+            rb2d.AddForce(new Vector2(-30, -15));
+        }
+    }
+}
---
+++ Breakout/Assets/Scripts/Bat.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+class Bat : MonoBehaviour
+{
+
+    // Defining variables to detect key inputs
+    public KeyCode moveLeftKey = KeyCode.LeftArrow;
+    public KeyCode moveRightKey = KeyCode.RightArrow;
+
+    // Boolean variables to detect if the bat can move or not
+    bool canMoveLeft = true;
+    bool canMoveRight = true;
+
+    // determines speed and default direction of the paddle
+    public float speed = 0.2f;
+    float direction = 0.0f;
+
+   
+    
+
+
+    void FixedUpdate()
+    {
+
+        Vector3 position = transform.localPosition;
+        position.x += speed * direction;
+        transform.localPosition = position;
+
+    }
+
+
+    
+    void Update()
+    {
+
+     
+        
+        // Detects if key inputs are made 
+        bool isLeftPressed = Input.GetKey(moveLeftKey);
+        bool isRightPressed = Input.GetKey(moveRightKey);
+
+       
+        // For moving left
+        if (isLeftPressed && canMoveLeft)
+        {
+            direction = -1.0f;
+        }
+
+        // For moving right
+        else if (isRightPressed && canMoveRight)
+        {
+            direction = 1.0f;
+        }
+
+        // For not moving
+        else
+        {
+            direction = 0.0f;
+        }
+
+    }
+
+
+    // detects collisions against walls
+    void OnCollisionEnter2D(Collision2D other)
+    {
+
+        // to check if the bat can move 
+        switch (other.gameObject.name)
+        {
+            case "Left Wall":
+                canMoveLeft = false;
+                break;
+
+            case "Right Wall":
+                canMoveRight = false;
+                break;
+        }
+
+    }
+
+
+    // collision detection against walls
+    void OnCollisionExit2D(Collision2D other)
+    {
+
+        // checks if bat is not touching walls 
+        switch (other.gameObject.name)
+        {
+            case "Left Wall":
+                canMoveLeft = true;
+                break;
+
+            case "Right Wall":
+                canMoveRight = true;
+                break;
+        }
+
+    }
+
+}
+    
+
